870dfaa5e2d5f23cc861c6a11be9f1626cd15bb6
david perrai <davidperrai@Mac.lan>
2026-01-28 23:21:46 +0100

    changement de la migration d'exemple pour un utilisateur

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
     .setDescription('The MyDives API description')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
