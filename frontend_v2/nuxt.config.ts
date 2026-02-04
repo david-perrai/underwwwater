84e8fd9cdda64dc33fc801188a2126afd83bcbcf
azimaet <maximedolo@gmail.com>
2026-02-04 13:25:09 +0100

    init. install primevue latest

--- frontend_v2/nuxt.config.ts
+++ frontend_v2/nuxt.config.ts
@@ -1,6 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxtjs/i18n']
-})
+  modules: ["@pinia/nuxt", "@nuxtjs/i18n"],
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "en",
+  },
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL,
+    },
+  },
+});
