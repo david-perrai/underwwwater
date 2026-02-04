c51458d8983e6cc2c2fe5fa1c2c1f5e148721c1d
azimaet <maximedolo@gmail.com>
2026-02-04 19:26:03 +0100

    init. scss files

---
+++ frontend_v2/app/components/_component.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+/** Props */
+
+/** Datas */
+
+/** Stores and Composables */
+
+/** Computeds */
+
+/** Functions */
+
+/** Lifecyle Hooks */
+</script>
+
+<template></template>
