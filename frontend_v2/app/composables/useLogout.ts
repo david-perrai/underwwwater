e78bc3e1bcd1222adba6537dc702ca496f388995
azimaet <maximedolo@gmail.com>
2026-02-08 22:45:35 +0100

    feat. add composables init

---
+++ frontend_v2/app/composables/useLogout.ts
@@ -0,0 +1,21 @@
+/**
+ * Logout Composable
+ * @description Add your composable description here
+ */
+export const useLogout = () => {
+  /** Stores */
+
+  /** State */
+
+  /** Computeds */
+
+  /** Methods */
+
+  /** Lifecycle Hooks */
+
+  /** Watchers */
+
+  return {
+    // Export your composable API here
+  };
+};
---
+++ frontend_v2/app/composables/usePostDive.ts
@@ -0,0 +1,21 @@
+/**
+ * PostDive Composable
+ * @description Add your composable description here
+ */
+export const usePostDive = () => {
+  /** Stores */
+
+  /** State */
+
+  /** Computeds */
+
+  /** Methods */
+
+  /** Lifecycle Hooks */
+
+  /** Watchers */
+
+  return {
+    // Export your composable API here
+  };
+};
