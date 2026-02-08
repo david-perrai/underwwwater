f3fcd9272aa6d465060ff210db775a2d110cddc0
david perrai <davidperrai@Mac.lan>
2026-02-08 22:55:55 +0100

    changement de nom des variables de base pour coller à PG

--- backend/src/configuration/data-source.ts
+++ backend/src/configuration/data-source.ts
@@ -4,11 +4,11 @@ config();
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.POSTGRES_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME || 'underwwwater_user',
-  password: process.env.DB_PASSWORD || 'underwwwater_password',
-  database: process.env.DB_NAME || 'underwwwater_db',
+  username: process.env.POSTGRES_USER || 'underwwwater_user',
+  password: process.env.POSTGRES_PASSWORD || 'underwwwater_password',
+  database: process.env.POSTGRES_DATABASE || 'underwwwater_db',
   entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*{.ts,.js}'],
   synchronize: false,
