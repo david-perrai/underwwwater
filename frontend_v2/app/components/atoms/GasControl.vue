3a075621ee8e10b93797829de5bb9172fc55821b
azimaet <maximedolo@gmail.com>
2026-02-15 19:59:01 +0100

    fix. control gas correction props

--- frontend_v2/app/components/atoms/GasControl.vue
+++ frontend_v2/app/components/atoms/GasControl.vue
@@ -32,29 +32,27 @@ const onToggleLock = (value: boolean) => {
 
 
     <div class="gas-control__inner">
-    <PVKnob 
-      :model-value="modelValue" 
-      :size="150" 
-      :min="0"
-      :max="100"
-      value-template="{value}%"
-      :value-color="color === 'primary' ? '#00FFEF' : color === 'secondary' ? '#244cff' : '#FFD600'"
-      :range-color="color === 'primary' ? 'rgba(0, 255, 239, 0.1)' : color === 'secondary' ? 'rgba(36, 76, 255, 0.1)' : 'rgba(255, 214, 0, 0.1)'"
-      :readonly="locked"
-      @update:model-value="onUpdateValue"
-    />
+      <PVKnob 
+        :model-value="modelValue" 
+        :size="150" 
+        :min="0"
+        :max="100"
+        value-template="{value}%"
+        :value-color="color === 'primary' ? '#00FFEF' : color === 'secondary' ? '#244cff' : '#FFD600'"
+        :range-color="color === 'primary' ? 'rgba(0, 255, 239, 0.1)' : color === 'secondary' ? 'rgba(36, 76, 255, 0.1)' : 'rgba(255, 214, 0, 0.1)'"
+        :readonly="locked"
+        @update:model-value="onUpdateValue"
+      />
 
-    <PVInputNumber 
-      :model-value="modelValue" 
-      mode="decimal" 
-      suffix="%" 
-      class="gas-control__input" 
-      input-class="gas-control__input-inner"
-      :min="0" 
-      :max="100"
-      :disabled="locked"
-      @update:model-value="(v) => onUpdateValue(v || 0)"
-    />
+      <PVInputNumber 
+        :model-value="modelValue" 
+        class="gas-control__input" 
+        input-class="gas-control__input-inner"
+        :min="0" 
+        :max="100"
+        :disabled="locked"
+        @update:model-value="(v) => onUpdateValue(v || 0)"
+      />
 
     </div>
 
