081b9791b1c9b8f77213b0b71aa0c380860f728f
azimaet <maximedolo@gmail.com>
2026-02-15 20:01:31 +0100

    feat. modals buckets (where put modals on the site)

--- frontend_v2/app/layouts/default.vue
+++ frontend_v2/app/layouts/default.vue
@@ -1,7 +1,24 @@
+<script setup lang="ts">
+import { useNavigationStore } from '~/stores/navigation';
+import Modal from '~/components/templates/Modal.vue';
+import FormDive from '~/components/organisms/FormDive.vue';
+
+const navigationStore = useNavigationStore();
+</script>
+
 <template>
   <div :class="['layout layout--default']">
     <Header />
 
     <slot />
+
+    <div id="modals" :class="['modals']" :data-id="'modals'">
+      <Modal 
+        v-if="navigationStore.isModalDiveVisible" 
+        @close="navigationStore.toggleModalDive()"
+      >
+        <FormDive />
+      </Modal>
+    </div>
   </div>
 </template>
