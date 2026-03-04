ac84864a77559ebed9cfd126d7f723a0ca6069c4
azimaet <maximedolo@gmail.com>
2026-03-04 13:18:33 +0100

    info. prepaer page accounts

---
+++ frontend/app/pages/account.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+/**
+ * TODO: Roadmap des fonctionnalités de la page Compte
+ * - [ ] Formulaire de changement de mot de passe
+ * - [ ] Formulaire de modification du profil (Username, Email, Avatar)
+ * - [ ] Paramètres de confidentialité (Compte privé/public)
+ * - [ ] Suppression définitive du compte
+ */
+</script>
+
+<template>
+  <main class="page-account">
+    <h1>Account</h1>
+    
+    <!-- TODO: Formulaire de modification du profil (Username, Email, Avatar) -->
+    
+    <!-- TODO: Formulaire de changement de mot de passe -->
+    
+    <!-- TODO: Paramètres de confidentialité (Compte privé/public) -->
+    
+    <!-- TODO: Suppression définitive du compte -->
+  </main>
+</template>
+
+<style scoped lang="scss">
+.page-account {}
+</style>
